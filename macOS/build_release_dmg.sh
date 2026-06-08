671722ed9ca58645a74564a84f338d49bbc032bb
Gurinder Singh <gurinder@coderabbit.ai>
2026-06-08 00:14:18 -0400

    fix

--- macOS/build_release_dmg.sh
+++ macOS/build_release_dmg.sh
@@ -127,9 +127,9 @@ cat > "${INFO_PLIST}" <<EOF
     <key>CFBundleIdentifier</key>
     <string>${BUNDLE_ID}</string>
     <key>CFBundleVersion</key>
-    <string>38</string>
+    <string>39</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0.37</string>
+    <string>1.0.38</string>
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>LSMinimumSystemVersion</key>
