03ad09482f07d330f0121d1f25c86a76384b8f8e
Gurinder Singh <gurinder@coderabbit.ai>
2026-07-05 20:18:59 -0400

    release: bump macOS to 1.1.0 (build 45), Windows to 1.16.0

--- macOS/build_release_dmg.sh
+++ macOS/build_release_dmg.sh
@@ -127,9 +127,9 @@ cat > "${INFO_PLIST}" <<EOF
     <key>CFBundleIdentifier</key>
     <string>${BUNDLE_ID}</string>
     <key>CFBundleVersion</key>
-    <string>44</string>
+    <string>45</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0.43</string>
+    <string>1.1.0</string>
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>LSMinimumSystemVersion</key>
