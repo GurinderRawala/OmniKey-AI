0d9250f960e22cd42249ee54e54de84005c7178f
Gurinder Singh <gurinder@coderabbit.ai>
2026-07-28 21:51:44 -0400

    bump version

--- macOS/build_release_dmg.sh
+++ macOS/build_release_dmg.sh
@@ -127,9 +127,9 @@ cat > "${INFO_PLIST}" <<EOF
     <key>CFBundleIdentifier</key>
     <string>${BUNDLE_ID}</string>
     <key>CFBundleVersion</key>
-    <string>50</string>
+    <string>51</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.4.0</string>
+    <string>1.4.1</string>
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>LSMinimumSystemVersion</key>
