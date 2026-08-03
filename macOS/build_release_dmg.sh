a0feff15e95ceae56c8fb6c6959aecf67828a790
Gurinder Singh <gurinder@coderabbit.ai>
2026-08-03 01:39:27 -0400

    release: bump macOS to 1.4.3 (build 53), Windows to 1.19.3

--- macOS/build_release_dmg.sh
+++ macOS/build_release_dmg.sh
@@ -127,9 +127,9 @@ cat > "${INFO_PLIST}" <<EOF
     <key>CFBundleIdentifier</key>
     <string>${BUNDLE_ID}</string>
     <key>CFBundleVersion</key>
-    <string>52</string>
+    <string>53</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.4.2</string>
+    <string>1.4.3</string>
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>LSMinimumSystemVersion</key>
