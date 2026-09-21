a5b1a1a8df95dc1baf8e70cd1f37cd484adf9eef
Gurinder Singh <gurinder@coderabbit.ai>
2026-09-21 14:15:01 -0400

    release(macOS): ship blue milestone markers in 1.9.3

--- api/src/index.ts
+++ api/src/index.ts
@@ -125,8 +125,8 @@ app.get('/macos/appcast', (req, res) => {
 
   // These should match the values embedded into the macOS app
   // Info.plist in macOS/build_release_dmg.sh.
-  const bundleVersion = '62';
-  const shortVersion = '1.9.2';
+  const bundleVersion = '63';
+  const shortVersion = '1.9.3';
 
   const xml = `<?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0"
